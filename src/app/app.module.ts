fa6c397e8567bb7057f1feee0f32ca0455704ba0
Aimen Azim <aimenazim504@gmail.com>
2023-04-29 06:38:18 +0500

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +20,17 @@ import { AboutMeComponent } from './about-me/about-me.component';
     HomeComponent,
     FooterComponent,
     ContactMeComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    WorkComponent,
+    HobbiesComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule
-  ],
+    ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faLocationDot, faPhone, faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.css']
 })
 export class ContactMeComponent implements OnInit {
-
+  icon1 = faLocationDot;
+  icon2 = faPhone;
+  icon3 = faEnvelope;
+  // icon4 = faGraduationCap;
+  // icon5 = faLayerGroup;
+  // icon6 = faCreditCard;
+  // icon7 = faSmile;
+  // icon8 = faBlog;
+  // icon9 = faAddressBook;
   constructor() { }
 
   ngOnInit(): void {
